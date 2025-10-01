80426947c4788fae87c705eead20e39a9e8e220e
Deni Andriansyah <148934716+DeniAndriansyah@users.noreply.github.com>
2025-10-01 20:25:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -100,12 +100,13 @@ def about_page():
     Aplikasi ini bekerja dengan cara memproses gambar daun jagung yang diambil melalui kamera dan memprediksi kategori penyakitnya.
     """)
 
-    st.header("Kelompok 10")
+    st.header("Kelompok 2")
     st.write("""
+    **Ketua Kelompok:**
+    1. Deni Andriansyah
     **Anggota Kelompok:**
-    1. M Rangga Saputra 221351100
-    2. Iqbal hiyatullah	221351059
-    3. M Tegar Yusuf Habibi 
+    2. Afip Dwi Cahyo
+    3. Melinda Purnama D  
     """)
 
     st.subheader("Tujuan Aplikasi")
